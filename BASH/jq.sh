69a9fb412e3993242a20649c793f21e211e6e5fe
kathryn.morgan <kathryn.morgan@braincraft.io>
2018-03-06 14:31:59 -0600

    working .json & jq query script

--- BASH/jq.sh
+++ BASH/jq.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # filter for packages in the class "monitoring"
 
+# Usage:
+# [command] [array_search_filter] [source_.json]
+#
+# Example:
+# $ ./jq.sh monitoring packages.json
+# 
+# Explanation:
+# [./jq.sh]       Run the jq.sh bash script
+# [monitoring]    Filter .json contents for the 'monitoring' class array
+# [packages.json] Name of json structured array index
+
 list_SOURCE=$2
 show_CLASS=$1
 
