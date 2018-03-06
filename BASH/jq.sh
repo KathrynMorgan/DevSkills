e8fd5d91f4a8267a383974e5ecb876706f73f5c4
kathryn.morgan <kathryn.morgan@braincraft.io>
2018-03-06 14:28:24 -0600

    working .json & jq query script

--- BASH/jq.sh
+++ BASH/jq.sh
@@ -6,4 +6,8 @@ show_CLASS=$1
 
 jq -r --arg search $show_CLASS '.[$search][] | "\(.name)"' "$list_SOURCE"
 
+# EXAMPLES:
+# jq -r --arg search $show_CLASS '.[$search][] | "\(.name)"' "$list_SOURCE"
+# jq -r --arg search $show_CLASS '.[$search][] | "\(.name) ==> \(.version)"' packages.json"'
+
 
