37dc1be6d161e96d2b0f3acdeb1ce6518933fb09
kathryn.morgan <kathryn.morgan@braincraft.io>
2018-03-06 14:25:02 -0600

    working .json & jq query script

--- BASH/jq.sh
+++ BASH/jq.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # filter for packages in the class "monitoring"
 
-list_SOURCE=$1
-show_CLASS=$2
+list_SOURCE=$2
+show_CLASS=$1
 
-jq -r '.monitoring[] | "\(.name)"' "$list_SOURCE"
+jq -r --arg search $show_CLASS '.[$search][] | "\(.name)"' "$list_SOURCE"
 
 
