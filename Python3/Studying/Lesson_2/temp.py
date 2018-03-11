0206f0731ef05df07ae0dc372a893290166435ea
kathryn.morgan <kathryn.morgan@braincraft.io>
2018-03-11 14:31:41 -0500

    hello-world.py interactive = working!

--- Python3/Studying/Lesson_2/temp.py
+++ Python3/Studying/Lesson_2/temp.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# Import libraries
+import os
+
 # Set Default Variables
 greetings = '''Hello, from here you can do a few things including:
     Send a "hello world message" [hello]
@@ -6,7 +9,8 @@ greetings = '''Hello, from here you can do a few things including:
 
 #################################################################################
 # Define the "hello" function
-def print_hello():
+def print_hello(usr_NAME):
+    os.system('clear')
     print('HELLO WERLD!')
     print('My name is ', usr_NAME)        # TODO: @stephanie why does this break
 
@@ -20,7 +24,7 @@ def req_command():
     while usr_CMD == "Empty":
         usr_CMD = input('What would you like to do {0}? : '.format(usr_NAME))
         if usr_CMD == 'hello':
-            print_hello()
+            print_hello(usr_NAME)
         else:
             print('Sorry, that command was not understood, please try again...')
 
