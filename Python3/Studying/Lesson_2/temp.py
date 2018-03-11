1afec9cab9696bf815269dd3b383462d4efeadc1
kathryn.morgan <kathryn.morgan@braincraft.io>
2018-03-11 17:48:06 -0500

    hello-world.py interactive + more improvements & hygene fixes

--- Python3/Studying/Lesson_2/temp.py
+++ Python3/Studying/Lesson_2/temp.py
@@ -4,7 +4,7 @@ import os
 
 #################################################################################
 # Define the "hello" function
-def print_hello_usr(usr_NAME, friend_NAME):
+def print_hello_usr(usr_NAME):
     os.system('clear')
     print('Hello', end=' ')
     print(friend_NAME, end='. ')
@@ -13,25 +13,37 @@ def print_hello_usr(usr_NAME, friend_NAME):
 
 #################################################################################
 # Define the "hello" function
-def print_hello_world(usr_NAME):
+def print_hello_world(usr_NAME, friend_NAME):
     os.system('clear')
     print('HELLO WERLD!', end=' ')
     print('My name is ', usr_NAME)
 
 #################################################################################
+# Ask friend what their name is
+def ask_friend_name():
+    os.system('clear')
+    friend_NAME = "None"
+    if friend_NAME == "None":           # TODO: change to while loop + add sanity check
+        friend_NAME = input('Hi Friend; what is your name? : ')
+
+#################################################################################
+# Ask friend what their name is
+def ask_usr_name():
+    os.system('clear')
+    usr_NAME = "None"
+    if usr_NAME == "None":              # TODO: change to while loop + add sanity check
+        usr_NAME = input('First, what is your name? : ')
+
+#################################################################################
 def req_command(usr_NAME):
     usr_CMD = "None"
-    if usr_NAME == "None":                         # TODO: change to while loop + add sanity check
-        usr_NAME = input('First, what is your name? : ')
-    print('Your name is:', usr_NAME)
-    if usr_CMD == "None":                          # TODO: change to while loop + add sanity check
+    if usr_CMD == "None":               # TODO: change to while loop + add sanity check
         usr_CMD = input('What would you like to do {0}? : '.format(usr_NAME))
-        if usr_CMD == 'hello world':               # TODO: change to while loop + add sanity check
+        if usr_CMD == 'hello world':    # TODO: change to while loop + add sanity check
+            ask_usr_name()
             print_hello_world(usr_NAME)
         elif usr_CMD == 'hello human':
-            friend_NAME = "None"
-            while friend_NAME == "None":
-                friend_NAME = input('Hi Friend; what is your name? : ')
+            ask_friend_name()
             print_hello_usr(usr_NAME, friend_NAME)
         else:
             print('Sorry, that command was not understood, please try again...')
@@ -40,7 +52,10 @@ def req_command(usr_NAME):
 # Leading function to define base variables etc.
 # Set Default Variables
 def main():
-    usr_NAME = "None"
+    try:
+        usr_NAME
+    except NameError:
+        usr_NAME = "None"
     greetings = '''Hello, from here you can do a few things including:
     Send a "hello world message"  [hello world]
     Send a "hello human message"  [hello human]
