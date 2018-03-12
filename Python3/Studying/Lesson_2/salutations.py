1e5ce34ee65b55e8066db07a6066df15c95db272
kathryn.morgan <kathryn.morgan@braincraft.io>
2018-03-11 21:21:01 -0500

    salutations.py breakout name requests into standalone functions

--- Python3/Studying/Lesson_2/salutations.py
+++ Python3/Studying/Lesson_2/salutations.py
@@ -4,6 +4,7 @@ import os
 
 #################################################################################
 # Define the "hello" function
+# TODO: clean up print to screen lines
 def print_hello_usr(usr_NAME, friend_NAME):
     os.system('clear')
     print('Hello', end=' ')
@@ -16,12 +17,13 @@ def print_hello_usr(usr_NAME, friend_NAME):
 def print_hello_world(usr_NAME):
     os.system('clear')
     print('>>  HELLO WERLD!', end=' ')
-    print('My name is', usr_NAME, end='   ;)')                         # BUG_1.2 - variable usr_NAME fails "not defined"
+    print('My name is', usr_NAME, end='<<   ;)')       # BUG_1.2 - variable usr_NAME fails "not defined"
 
 #################################################################################
 # Ask friend what their name is
 def ask_friend_name(friend_NAME):
     os.system('clear')
+    friend_NAME = "None"
     if friend_NAME == "None":                              # TODO: change to while loop + add sanity check
         friend_NAME = input('Hi Friend; what is your name? : ')
         return friend_NAME
@@ -31,8 +33,8 @@ def ask_friend_name(friend_NAME):
 def ask_usr_name(usr_NAME):
     os.system('clear')
     if usr_NAME == "None":                                 # TODO: change to while loop + add sanity check
-        usr_NAME = input('First, what is your name? : ')   # BUG_1.1 - Set variable 'usr_NAME'
-        return usr_NAME
+        usr_NAME = input('First, what is your name? : ')
+        return usr_NAME                              # BUG_1.1 - attempting to return variable 'usr_NAME'
 
 #################################################################################
 def req_command(usr_CMD, usr_NAME):
@@ -42,23 +44,15 @@ def req_command(usr_CMD, usr_NAME):
 
         if usr_CMD == 'hello world':                       # TODO: change to while loop + add sanity check
 
-            # Get User Name
-            ask_usr_name(usr_NAME)
-
-            # Print Salutations + User Name
-            print_hello_world(usr_NAME)
+            ask_usr_name(usr_NAME)                   # Get User Name
+            print_hello_world(usr_NAME)              # Print Salutations + User Name
 
         elif usr_CMD == 'hello human':
 
-            # Get User Name
-            ask_usr_name(usr_NAME)
+            ask_usr_name(usr_NAME)                   # Get User Name
+            ask_friend_name()                        # Get Friend Name
 
-            # Get Friend Name
-            friend_NAME = "None"
-            ask_friend_name(friend_NAME)
-
-            # Print Salutations + User Name
-            print_hello_usr(usr_NAME, friend_NAME)
+            print_hello_usr(usr_NAME, friend_NAME)   # Print Salutations + User Name
 
         else:
             print('Sorry, that command was not understood, please try again...')
